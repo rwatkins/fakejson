c60114181ea3db20829823dcc961ee615ffef84b
Riley Watkins <riley@rileywatkins.com>
2014-10-09 20:44:44 -0700

    Add parse_string()

--- fakejson.py
+++ fakejson.py
@@ -34,30 +34,38 @@ def parse_array(s, end=0):
         if nextchar == '"':
             # start parsing string
             print 'parsing string'
-            str_value = ''
-            end += 1
-            nextchar = s[end:end+1]
-            escape = False
-            while nextchar:
-                print nextchar
-                if nextchar == '\\' and not escape:
-                    print 'found escape character'
-                    escape = True
-                    end += 1
-                    nextchar = s[end:end+1]
-                    continue
-                if nextchar == '"':
-                    if escape:
-                        escape = False
-                    else:
-                        break
-                str_value += nextchar
-                end += 1
-                nextchar = s[end:end+1]
-            print 'appending string: %s' % str_value
-            values.append(str_value)
+            st, end = parse_string(s, end=end)
+            values.append(st)
         end += 1
         nextchar = s[end:end+1]
+
+
+def parse_string(s, end=0):
+    assert s, "s is empty"
+    nextchar = s[end:end+1]
+    assert nextchar == '"', 'parse_string() called for a non-string'
+    value = ''
+    end += 1
+    nextchar = s[end:end+1]
+    escape = False
+    while nextchar:
+        print nextchar
+        if nextchar == '\\' and not escape:
+            print 'found escape character'
+            escape = True
+            end += 1
+            nextchar = s[end:end+1]
+            continue
+        if nextchar == '"':
+            if escape:
+                escape = False
+            else:
+                break
+        value += nextchar
+        end += 1
+        nextchar = s[end:end+1]
+    print 'parsed string: %s' % value
+    return value, end
 
 
 def is_whitespace(char):
